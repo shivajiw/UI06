c718efb32e6100f776614f833461cfbee3b9db89
shivajiw <shiva.watekar@gmail.com>
2018-11-24 15:08:50 +0530

    Class on 24/11/2018
    Routing, ngFor, ngClass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 import { MyComComponent } from './my-comp/MyCom.component';
-
 import { MyTest } from './my-test/my-test';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { DirectiveComponent } from './directive/directive.component';
+import { NavComponent } from './nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: 'first', component: FirstComponent },
+  { path: 'directive', component: DirectiveComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComComponent, 
-    MyTest
+    MyTest, 
+    FirstComponent, DirectiveComponent, NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
